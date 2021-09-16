dc4e863e2ab96cb9371bd3704c649641fb9cc764
Thiago Felipe <thiagofelipe1311@gmail.com>
2021-09-16 16:55:22 -0300

    Update 7.27(A).c

--- 7.27(A).c
+++ 7.27(A).c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-
-#define TAM 5
-// Definição dos comandos
-//-------------------------------------------------------------------------
-#define READ 10
-#define WRITE 11
-#define ADD 30
-#define HALT 43
-//-------------------------------------------------------------------------
+#define TAM 10
+//Operações de entrada/saída:
+#define READ 10 //Lê uma palavra do terminal para um local específico na memória.
+#define WRITE 11 //Escreve uma palavra de um local específico na memória para o terminal.
+//Operações de carregamento/armazenamento:
+#define LOAD 20 //Carrega uma palavra de um local específico na memória para o acumulador.
+#define STORE 21 //Armazena uma palavra do acumulador para um local específico na memória.
+//Operações aritméticas:
+#define ADD 30 //Soma uma palavra de um local específico na memória à palavra no acumulador (deixa o resultado no acumulador).
+#define SUBTRACT 31 //Subtrai uma palavra de um local específico na memória da palavra no acumulador (deixa o resultado no acumulador).
+#define DIVIDE 32 //Divide uma palavra de um local específico na memória pela palavra no acumulador (deixa o resultado no acumulador).
+#define MULTIPLY 33 //Multiplica uma palavra de um local específico na memória pela palavra no acumulador (deixa o resultado no acumulador).
+//Operações de transferência de controle:
+#define BRANCH 40 //Desvia para um local específico na memória.
+#define BRANCHNEG 41 //Desvia para um local específico na memória se o acumulador for negativo.
+#define BRANCHZERO 42 //Desvia para um local específico na memória se o acumulador for zero.
+#define HALT 43 //Para (halt), ou seja, o programa concluiu sua tarefa.
 int main(){
     setlocale(LC_ALL, "Portuguese");
-
     int i,j, maior = 0;
-    int comando[10];
-    char sinal[10];
+    int comando[100];
+    char sinal[100];
     int operando, operar;
-
+    int sentinela = 0;
     printf("*** Bem vindo ao Simpletron!                        ***\n");
     printf("*** Favor digitar seu programa, uma instrução       ***\n");
     printf("*** (ou palavra de dados) por vez. Mostrarei        ***\n");
@@ -25,14 +32,11 @@ int main(){
     printf("*** Você, então, deverá digitar a palavra para esse ***\n");
     printf("*** local. Digite a sentinela +4300 para            ***\n");
     printf("*** encerrar a entrada do seu programa.             ***\n");
-
-    for(i = 0; i < TAM; i++){
+    for(i = 0; i <= TAM; i++){
         fflush(stdin);
         scanf("%c%4d", &sinal[i], &comando[i]);
-
         operando = comando[i]%100;
         operar = comando[i]/100;
-
         if (operando > maior){
             maior = operando;
         }
@@ -40,53 +44,81 @@ int main(){
             break;
         }
     }
-
-    for(j = 0; j < i; j++){
-        printf("%02d \? %c%4d\n", j, sinal[j],comando[j]);
+    sentinela = maior;
+    for(j = 0; j <= sentinela; j++){
+        if (j < i){
+            printf("%02d \? %c%4d\n", j, sinal[j],comando[j]);
+        }else{
+            sinal[j] = '+';
+            comando[j] = 0000;
+            printf("%02d \? %c%04d\n", j, sinal[j],comando[j]);
+        }
+        
     }
     printf("*** Carga do programa concluída    ***\n");
     printf("*** Iniciando execução do programa ***\n");
-
     system("pause");
-
     int valores_lidos[maior];
-    char sinal_de_valores[10];
     int acumulador = 0;
-
-    for (i = 0; i <= maior; i++){
-        valores_lidos[i] = 0;
+    for (j = 0; j <= maior; j++){
+        valores_lidos[j] = 0;
     }
+    for (j = 0; j < sentinela; j++){
 
-    for (i = 0; i < TAM; i++){
-
-        operando = comando[i]%100;
-        operar = comando[i]/100;
-
-        switch (operar)
-        {
-        case READ:
-            fflush(stdin);
-            printf("Digite o número para posição %02d:\t", operando);
-            scanf("%d", &valores_lidos[operando]);
-            printf("\n");
-            break;
-        case WRITE:
-            printf("O número de posição %02d:\t%+d\n", operando, valores_lidos[operando]);
-            break;
-        case ADD:
-            for (i = 0; i <= maior; i++){
-                acumulador += valores_lidos[i];
-            }
-            printf("Soma de todos os números inseridos:\t%+d\n", acumulador);
-            break;
-        case HALT:
-            break;
+        operar = 0;
+        operando = 0;
+        
+        operando = comando[j]%100;
+        operar = comando[j]/100;
+        switch (operar){
+            case READ:
+                printf("Informe o valor com endereço a linha %02d: ", operando);
+                scanf("%d", &valores_lidos[operando]);
+                break;
+            case WRITE:
+                printf("Valor na memória da linha %02d: %d\n", operando, valores_lidos[operando]);
+                break;
+            case LOAD:
+                acumulador = valores_lidos[operando];
+                break;
+            case STORE:
+                valores_lidos[operando] = acumulador;
+                break;
+            case ADD:
+                acumulador += valores_lidos[operando];
+                break;
+            case SUBTRACT:
+                acumulador -= valores_lidos[operando];
+                break;
+            case DIVIDE:
+                acumulador /= valores_lidos[operando];
+                break;
+            case MULTIPLY:
+                acumulador *= valores_lidos[operando];
+                break;
+            case BRANCH:
+                j = operando;
+                continue;
+                break;
+            case BRANCHNEG:
+                if (acumulador < 0){
+                    j = operando;
+                    continue;
+                }
+                break;
+            case BRANCHZERO:
+                if (acumulador == 0){
+                    j = operando;
+                    continue;
+                } 
+                break;
+            case HALT: 
+                break;
         }
-        if (comando[i] == HALT){
+        if (comando[j] == HALT){
             printf("*** Execução do Simpletron encerrada ***\n");
             break;
         }
     }
-    
     return 0;
 }
