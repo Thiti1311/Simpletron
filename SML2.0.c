0f63849d0c30a793694c9070ebc36e464c2d3434
Thiago Felipe <thiagofelipe1311@gmail.com>
2021-09-28 12:43:04 -0300

    Update SML2.0.c

--- SML2.0.c
+++ SML2.0.c
@@ -1,43 +1,52 @@
+  
 #include <stdio.h>
 #include <string.h>
 #include <stdio.h>
 #include <locale.h>
+#include <time.h>
 
 #define VAL 00
-//Operaï¿½ï¿½es de entrada/saï¿½da:
-#define READ 10 //Lï¿½ uma palavra do terminal para um local especï¿½fico na memï¿½ria.
-#define WRITE 11 //Escreve uma palavra de um local especï¿½fico na memï¿½ria para o terminal.
-//Operaï¿½ï¿½es de carregamento/armazenamento:
-#define LOAD 20 //Carrega uma palavra de um local especï¿½fico na memï¿½ria para o accumulator.
-#define STORE 21 //Armazena uma palavra do accumulator para um local especï¿½fico na memï¿½ria.
-//Operaï¿½ï¿½es aritmï¿½ticas:
-#define ADD 30 //Soma uma palavra de um local especï¿½fico na memï¿½ria ï¿½ palavra no accumulator (deixa o resultado no accumulator).
-#define SUBTRACT 31 //Subtrai uma palavra de um local especï¿½fico na memï¿½ria da palavra no accumulator (deixa o resultado no accumulator).
-#define DIVIDE 32 //Divide uma palavra de um local especï¿½fico na memï¿½ria pela palavra no accumulator (deixa o resultado no accumulator).
-#define MULTIPLY 33 //Multiplica uma palavra de um local especï¿½fico na memï¿½ria pela palavra no accumulator (deixa o resultado no accumulator).
-//Operaï¿½ï¿½es de transferï¿½ncia de controle:
-#define BRANCH 40 //Desvia para um local especï¿½fico na memï¿½ria.
-#define BRANCHNEG 41 //Desvia para um local especï¿½fico na memï¿½ria se o accumulator for negativo.
-#define BRANCHZERO 42 //Desvia para um local especï¿½fico na memï¿½ria se o accumulator for zero.
+//Operaes de entrada/sada:
+#define READ 10 //L uma palavra do terminal para um local especfico na memria.
+#define WRITE 11 //Escreve uma palavra de um local especfico na memria para o terminal.
+//Operaes de carregamento/armazenamento:
+#define LOAD 20 //Carrega uma palavra de um local especfico na memria para o accumulator.
+#define STORE 21 //Armazena uma palavra do accumulator para um local especfico na memria.
+//Operaes aritmticas:
+#define ADD 30 //Soma uma palavra de um local especfico na memria  palavra no accumulator (deixa o resultado no accumulator).
+#define SUBTRACT 31 //Subtrai uma palavra de um local especfico na memria da palavra no accumulator (deixa o resultado no accumulator).
+#define DIVIDE 32 //Divide uma palavra de um local especfico na memria pela palavra no accumulator (deixa o resultado no accumulator).
+#define MULTIPLY 33 //Multiplica uma palavra de um local especfico na memria pela palavra no accumulator (deixa o resultado no accumulator).
+//Operaes de transferncia de controle:
+#define BRANCH 40 //Desvia para um local especfico na memria.
+#define BRANCHNEG 41 //Desvia para um local especfico na memria se o accumulator for negativo.
+#define BRANCHZERO 42 //Desvia para um local especfico na memria se o accumulator for zero.
 #define HALT 43 //Para (halt), ou seinstructionCountera, o programa concluiu sua tarefa.
 int menu_mostrar();
 void sml_menu();
 int validar_operador(int num); 
+int verificar_existencia(int valor, char string);
 
 int main(){
-    //setlocale(LC_ALL, "Portuguese");
+    setlocale(LC_ALL, "Portuguese");
 
     int num;
     int instructionCounter;
     int memory[100];
-    int num_Arquivo = 1;
+    int num_Arquivo;
     int i=0;
     char sinal[100];
     int operacao[100];
     int operationCode;
     int verificar;
-    char nome[10] = "PROG", arquivo_d[10];
+    char nome[10] = "PROG", arquivo_d[15];
     char aux[4], tamanho;
+    char c;
+
+    struct tm *data_hora_atual;     
+    time_t segundos;
+    time(&segundos);   
+    data_hora_atual = localtime(&segundos); 
 
     FILE *arquivo;
 
@@ -103,12 +112,12 @@ int main(){
                     instructionCounter++;
                 }
                 i = 0;
+                fprintf(arquivo, "%02d:%02d %02d/%02d/%4d\n", data_hora_atual->tm_hour,data_hora_atual->tm_min, data_hora_atual->tm_mday, data_hora_atual->tm_mon+1,data_hora_atual->tm_year+1900);
                 while(i < instructionCounter){
-                    printf("%.2d \? ", i);
-                    printf("%c%4d\n", sinal[i], memory[i]);
+                    //printf("%.2d \? ", i);
+                    //printf("%c%4d\n", sinal[i], memory[i]);
                     fprintf(arquivo, "%.2d \? %c%.4d\n", i, sinal[i], memory[i]);
                     i++;
-
                 }
                 printf("\n* Carga do programa concluida    *\n");
                 rewind( arquivo );
@@ -119,7 +128,7 @@ int main(){
             
             break;
         case 3:
-            printf("\nEscolha o arquivo para pesquisa/leitura: ");
+            printf("Informe o nome do arquivo para pesquisa/leitura: ");
             scanf("%s", arquivo_d);
             
             arquivo = fopen(arquivo_d, "r");
@@ -129,17 +138,17 @@ int main(){
             else
             {
                 printf("O arquivo foi encontrado!\n");
-//FSCANF TA CERTO?
-                printf( "%-6s%s\n", "Local", "Numero");
-                fscanf(arquivo, "%.2d%c%.4d\n", i, sinal[i], memory[i]);
-                i = 0;
-                while ( !feof( arquivo ) ) {
-                    printf("%5d ", i);
-                    printf("%c%4d\n", sinal[i], memory[i]);
-                    fprintf(arquivo, "%.2d %c%.4d\n", i, sinal[i], memory[i]);
-                    i++;
-
+            
+            c = fgetc(arquivo);
+            while (c != EOF)
+                {
+                    printf ("%c", c);
+                    c = fgetc(arquivo);
                 }
+                printf("\n");
+            
+                fclose(arquivo);
+            
             }
             rewind( arquivo );
             fclose(arquivo);
@@ -219,4 +228,33 @@ int validar_operador(int num){
             default:
             return 1;
     }
+}
+int verificar_existencia(int valor, char string){
+    int tam;
+    char aux;
+    FILE *arquivo;
+        itoa(valor, aux, 10);
+            tam = strlen(aux);
+            switch (tam)
+            {
+            case 1:
+                strcat(string, "00");
+                strcat(string, aux);
+                strcat(string, ".txt");
+                break;
+            case 2:
+                strcat(string, "0");
+                strcat(string, aux);
+                strcat(string, ".txt");
+                break;
+            case 3:
+                strcat(string, aux);
+                strcat(string, ".txt");
+                break;
+            }
+    arquivo = fopen(string, "r");
+    while ((arquivo = fopen(string, "r"))!= NULL){
+        valor++;
+    }
+    return valor;
 }
